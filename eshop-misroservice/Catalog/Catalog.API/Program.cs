a7a68befd4be46260df81bc54b30b60da1b0446d
ihrishad <rishad.ismailhossain@gmail.com>
2025-01-18 14:17:39 +0600

    building stricture for CQRS Architecture

--- eshop-misroservice/Catalog/Catalog.API/Program.cs
+++ eshop-misroservice/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // add services to container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP Request Pipeline
+app.MapCarter();
 
 app.Run();
